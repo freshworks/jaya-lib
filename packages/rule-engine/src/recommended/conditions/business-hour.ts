e7f74e0e25d6bede69e825b1f365ad42f4bb74a7
abisht1991 <ajay.bisht@freshworks.com>
2020-04-07 11:39:23 +0530

    conditionBusinessHour initial commit

--- packages/rule-engine/src/recommended/conditions/business-hour.ts
+++ packages/rule-engine/src/recommended/conditions/business-hour.ts
@@ -1,13 +1,21 @@
 import { Condition } from "../../models/rule";
 import { ProductEventData } from "@freshworks-jaya/marketplace-models";
 import { Utils } from '../../Utils';
+import { Integrations } from '../../models/rule-engine';
+import axios from 'axios';
 
-export default (condition: Condition, productEventData: ProductEventData): boolean => {
-  const modelProperties = productEventData.conversation || productEventData.message;
+export default async (condition: Condition, productEventData: ProductEventData, integrations: Integrations,): boolean => {
+  
 
-  return Utils.evaluateCondition(
-    condition.operator,
-    modelProperties.channel_id,
-    condition.value as string
-  );
+  const freshchatApiUrl= integrations.freshchat.v1.url;
+  const freshchatApiToken= integrations.freshchat.v1.token;
+  
+  const businessHours = await axios.get(freshchatApiUrl, {
+    headers: {
+      Authorization: freshchatApiToken,
+    },
+  });
+  let conditionBusinessHour = businessHours.data.filterBy('operatingHoursId', condition.value);
+
+  // call business hour operator with conditionBusinessHour and return the same
 }
