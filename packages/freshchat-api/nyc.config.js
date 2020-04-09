441a824d99ebab0dadf206e58d02455691b34ba8
Arun Rajkumar <arun.rajkumar@freshworks.com>
2020-04-09 10:30:03 +0530

    Added code coverage thresholds for test cases to pass

---
+++ packages/freshchat-api/nyc.config.js
@@ -0,0 +1,9 @@
+'use strict';
+module.exports = {
+  branches: 100,
+  'check-coverage': true,
+  extends: '@istanbuljs/nyc-config-typescript',
+  functions: 100,
+  lines: 100,
+  statements: 100,
+};
---
+++ packages/rule-engine/nyc.config.js
@@ -0,0 +1,9 @@
+'use strict';
+module.exports = {
+  branches: 78,
+  'check-coverage': true,
+  extends: '@istanbuljs/nyc-config-typescript',
+  functions: 80,
+  lines: 80,
+  statements: 80,
+};
