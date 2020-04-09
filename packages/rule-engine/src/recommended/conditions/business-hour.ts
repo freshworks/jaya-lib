51e34321650b9a579aa86a47763d2d1a9cb860e7
abisht1991 <ajay.bisht@freshworks.com>
2020-04-09 14:32:27 +0530

    sending operand2 as '' in businesshour condition

--- packages/rule-engine/src/recommended/conditions/business-hour.ts
+++ packages/rule-engine/src/recommended/conditions/business-hour.ts
@@ -4,10 +4,5 @@ import { Utils } from '../../Utils';
 import { Integrations } from '../../models/rule-engine';
 
 export default (condition: Condition, productEventData: ProductEventData, integrations: Integrations): boolean => {
-  return Utils.evaluateCondition(
-    condition.operator,
-    productEventData.associations.channel.id,
-    condition.value as string,
-    integrations,
-  );
+  return Utils.evaluateCondition(condition.operator, condition.value as string, '', integrations);
 };
