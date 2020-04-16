e637bf0e2772d7c962f8d65bd13acd948b8804a2
abisht1991 <ajay.bisht@freshworks.com>
2020-04-16 19:27:28 +0530

    v2 api changes

--- packages/rule-engine/src/recommended/actions/reopen.ts
+++ packages/rule-engine/src/recommended/actions/reopen.ts
@@ -7,8 +7,8 @@ export default (
   productEventData: ProductEventData,
   actionValue: unknown,
 ): Promise<unknown> => {
-  const freshchatApiUrl = integrations.freshchatv1.url;
-  const freshchatApiToken = integrations.freshchatv1.token;
+  const freshchatApiUrl = integrations.freshchatv2.url;
+  const freshchatApiToken = integrations.freshchatv2.token;
   const freshchat = new Freshchat(freshchatApiUrl, freshchatApiToken);
   const modelProperties = productEventData.conversation || productEventData.message;
   const conversationId = modelProperties.conversation_id;
