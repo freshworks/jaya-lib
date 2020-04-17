52cca117af57f4257c21f46c9a29a72eed2d493e
abisht1991 <ajay.bisht@freshworks.com>
2020-04-17 17:35:59 +0530

    0.0.47-aplhpa.11 changes

--- packages/rule-engine/src/recommended/operators/within-business-hours.ts
+++ packages/rule-engine/src/recommended/operators/within-business-hours.ts
@@ -5,7 +5,8 @@ import { isOutsideBusinessHours } from '@freshworks-jaya/utilities';
 export default async (op1: string, op2: string, integrations: Integrations): Promise<void> => {
   try {
     const businessHour = await Utils.getBusinessHour(op1, integrations);
-    return Utils.promisify(!isOutsideBusinessHours(businessHour, new Date().getTime()));
+    const isOutsideBusinessHoursValue = isOutsideBusinessHours(businessHour, new Date().getTime());
+    return Utils.promisify(!isOutsideBusinessHoursValue);
   } catch (err) {
     if (err) {
       throw new Error('Error while getting BusinessHour');
