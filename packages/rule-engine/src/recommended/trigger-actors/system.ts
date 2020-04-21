0f5b46df49b1539e127265a636de40615802a305
Arun Rajkumar <arun.rajkumar@freshworks.com>
2020-04-21 19:29:55 +0530

    Refactored system.ts

--- packages/rule-engine/src/recommended/trigger-actors/system.ts
+++ packages/rule-engine/src/recommended/trigger-actors/system.ts
@@ -1,25 +1,39 @@
 import { ProductEventData, ActorType, Actor, ActorSubType } from '@freshworks-jaya/marketplace-models';
 import { TriggerActor, TriggerActorCause } from '../../models/rule';
 
-// eslint-disable-next-line complexity
 export default (productEventData: ProductEventData, triggerActor: TriggerActor): boolean => {
   const isActorTypeMatch = productEventData.actor.type === ActorType.System;
   // Todo: check for cause if it is present
   let isActorCauseMatch = true;
 
-  if (triggerActor.cause) {
-    isActorCauseMatch =
-      (triggerActor.cause === TriggerActorCause.IntelliAssign &&
-        productEventData.actor.sub_type === ActorSubType.Intelliassign) ||
-      (triggerActor.cause === TriggerActorCause.AssignmentRule &&
-        productEventData.actor.sub_type === ActorSubType.AssignmentRule) ||
-      (triggerActor.cause === TriggerActorCause.AgentGroupMapping &&
-        productEventData.actor.sub_type === ActorSubType.AgentGroupMapping) ||
-      (triggerActor.cause === TriggerActorCause.User && productEventData.actor.sub_type === ActorSubType.User) ||
-      (triggerActor.cause === TriggerActorCause.Agent && productEventData.actor.sub_type === ActorSubType.Agent) ||
-      (triggerActor.cause === TriggerActorCause.AutoResolve &&
-        productEventData.actor.sub_type === ActorSubType.AutoResolve) ||
-      (triggerActor.cause === TriggerActorCause.Bot && productEventData.actor.sub_type === ActorSubType.Bot);
+  const actorCauseDict: {
+    [key in TriggerActorCause]: (actorSubType: ActorSubType) => boolean;
+  } = {
+    [TriggerActorCause.Agent]: (actorSubType: ActorSubType): boolean => {
+      return actorSubType === ActorSubType.Agent;
+    },
+    [TriggerActorCause.AgentGroupMapping]: (actorSubType: ActorSubType): boolean => {
+      return actorSubType === ActorSubType.AgentGroupMapping;
+    },
+    [TriggerActorCause.AssignmentRule]: (actorSubType: ActorSubType): boolean => {
+      return actorSubType === ActorSubType.AssignmentRule;
+    },
+    [TriggerActorCause.AutoResolve]: (actorSubType: ActorSubType): boolean => {
+      return actorSubType === ActorSubType.AutoResolve;
+    },
+    [TriggerActorCause.Bot]: (actorSubType: ActorSubType): boolean => {
+      return actorSubType === ActorSubType.Bot;
+    },
+    [TriggerActorCause.IntelliAssign]: (actorSubType: ActorSubType): boolean => {
+      return actorSubType === ActorSubType.Intelliassign;
+    },
+    [TriggerActorCause.User]: (actorSubType: ActorSubType): boolean => {
+      return actorSubType === ActorSubType.User;
+    },
+  };
+
+  if (triggerActor.cause && productEventData.actor['sub_type'] && actorCauseDict[triggerActor.cause]) {
+    isActorCauseMatch = actorCauseDict[triggerActor.cause](productEventData.actor.sub_type);
   }
 
   return isActorTypeMatch && isActorCauseMatch;
