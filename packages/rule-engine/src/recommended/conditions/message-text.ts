1b59249100bc5fadb8afdd3f277b8bbb7121a1cb
Arun Rajkumar <arun.rajkumar@freshworks.com>
2020-05-11 13:11:54 +0530

    Revert "Updated rule engine"
    This reverts commit 4be12cc5243358d3c6a3bca3a58e30052192f9b3.

--- packages/rule-engine/src/recommended/conditions/message-text.ts
+++ packages/rule-engine/src/recommended/conditions/message-text.ts
@@ -1,22 +1,7 @@
-import { ProductEventData, MessagePart } from '@freshworks-jaya/marketplace-models';
+import { ProductEventData } from '@freshworks-jaya/marketplace-models';
 import { Condition } from '../../models/rule';
 import { Utils } from '../../Utils';
 import { Integrations } from '../../models/rule-engine';
-
-const getMessagePartsTextContent = (messageParts: MessagePart[]): string => {
-  let messageContent = '';
-
-  if (messageParts && messageParts.length) {
-    messageContent = messageParts
-      .filter((messagePart) => messagePart.text)
-      .map((messagePart) => {
-        return messagePart.text && messagePart.text.content;
-      })
-      .join(' ');
-  }
-
-  return messageContent;
-};
 
 export default (
   condition: Condition,
@@ -27,7 +12,7 @@ export default (
 
   return Utils.evaluateCondition(
     condition.operator,
-    getMessagePartsTextContent(modelProperties.messages[0].message_parts),
+    Utils.getMessagePartsTextContent(modelProperties.messages[0].message_parts),
     condition.value as string,
     integrations,
   );
