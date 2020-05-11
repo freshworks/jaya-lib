5c2e9f743c33135f872907026b1d973b022fc9c9
Arun Rajkumar <arun.rajkumar@freshworks.com>
2020-05-11 13:16:39 +0530

    FIxed major bug

--- packages/rule-engine/src/RuleProcessor.ts
+++ packages/rule-engine/src/RuleProcessor.ts
@@ -42,9 +42,13 @@ export class RuleProcessor {
           block.conditions.map((condition) => {
             return this.isConditionMatching(productEventData, condition, integrations);
           }),
-        ).then(() => {
-          return Promise.resolve();
-        });
+        )
+          .then(() => {
+            return Promise.resolve();
+          })
+          .catch(() => {
+            return Promise.reject('Some conditions did not match');
+          });
       case MatchType.Any:
         return Promise.any(
           block.conditions.map((condition) => {
@@ -123,9 +127,13 @@ export class RuleProcessor {
           rule.blocks.map((block) => {
             return this.isBlockMatching(productEventData, block, integrations);
           }),
-        ).then(() => {
-          return Promise.resolve();
-        });
+        )
+          .then(() => {
+            return Promise.resolve();
+          })
+          .catch(() => {
+            return Promise.reject('Some blocks did not match');
+          });
       case MatchType.Any:
         return Promise.any(
           rule.blocks.map((block) => {
