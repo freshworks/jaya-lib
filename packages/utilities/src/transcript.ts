401b7381e106198a45f6c76d02485b9e15e2b155
Arun Rajkumar <arun.rajkumar@freshworks.com>
2020-05-14 19:22:32 +0530

    Test commit for handlebars support

---
+++ packages/utilities/src/transcript.ts
@@ -0,0 +1,8 @@
+// import Handlebars from 'handlebars';
+// import helpers from 'handlebars-helpers';
+
+// const template = Handlebars.compile('Name: {{name}}');
+// // eslint-disable-next-line no-console
+// console.log(template({ name: 'Nils' }));
+
+// helpers();
