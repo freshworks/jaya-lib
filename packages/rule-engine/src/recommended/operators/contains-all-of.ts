8dc7180a13736d6db9b43bb2cfb5da7b1c38cc04
srivatsan7 <srivatsan.v@freshworks.com>
2023-08-22 20:46:15 +0530

    Update contains-all-of.ts

--- packages/rule-engine/src/recommended/operators/contains-all-of.ts
+++ packages/rule-engine/src/recommended/operators/contains-all-of.ts
@@ -3,10 +3,9 @@ import { Utils } from '../../Utils';
 
 export default (op1: string, op2: string, integrations: Integrations, options: RuleEngineOptions): Promise<void> => {
   const operand1 = JSON.parse(op1) as string[];
-  const operand2 = JSON.parse(op2) as string[];
-
-  const containsAll = operand1.every((element) => {
-    return operand2.includes(element);
+  const operand2 = op2.split(',');
+  const containsAll = operand2.every((element) => {
+    return operand1.includes(element);
   });
   return Utils.promisify(containsAll);
 };
