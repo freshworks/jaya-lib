655a11bcf91b41f338fa014595c930b9f20906dc
ShashaankKrishnatray <shashaank.krishnatray@freshworks.com>
2024-09-04 11:19:55 +0530

    channel changes

---
+++ packages/rule-engine/src/recommended/conditions/channel-type.ts
@@ -0,0 +1,21 @@
+import { ProductEventData } from '@freshworks-jaya/marketplace-models';
+import { Condition } from '../../models/rule';
+import { Utils } from '../../Utils';
+import { Integrations, RuleEngineOptions } from '../../models/rule-engine';
+
+export default (
+  condition: Condition,
+  productEventData: ProductEventData,
+  integrations: Integrations,
+  options: RuleEngineOptions,
+  ruleAlias: string,
+): Promise<void> => {
+  return Utils.evaluateCondition(
+    condition.operator,
+    productEventData?.conversation?.source || productEventData?.message?.source ||'',
+    condition.value as string,
+    integrations,
+    options,
+    ruleAlias,
+  );
+};
