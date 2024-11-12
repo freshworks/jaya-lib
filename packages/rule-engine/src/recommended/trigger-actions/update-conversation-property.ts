ea914ca075d7b1859b3affdf1f0c8b811ab558d6
ShashaankKrishnatray <shashaank.krishnatray@freshworks.com>
2024-11-12 10:46:19 +0530

    convo props update check

--- packages/rule-engine/src/recommended/trigger-actions/update-conversation-property.ts
+++ packages/rule-engine/src/recommended/trigger-actions/update-conversation-property.ts
@@ -2,18 +2,17 @@ import { Event, ProductEventData } from '@freshworks-jaya/marketplace-models';
 import { TriggerAction } from '../../models/rule';
 
 export default (productEvent: Event, productEventData: ProductEventData, triggerAction: TriggerAction): boolean => {
-  const keys = Object.keys(productEventData.changes.model_changes);
-  const match = keys.find((value) => /^cf_[a-zA-Z0-9_]+/.test(value));
+  const { actor, changes } = productEventData;
+  const { model_changes } = changes || {};
+  const { assigned_agent_id, assigned_group_id, status, priority } = model_changes || {};
+
+  const match = Object.keys(model_changes).find((key) => /^cf_[a-zA-Z0-9_]+/.test(key));
 
   return (
     productEvent === Event.ConversationUpdate &&
-    productEventData.actor?.actor_source != null &&
-    productEventData.actor?.actor_source != 'API' &&
-    !!productEventData.changes.model_changes &&
-    (!!productEventData.changes.model_changes.assigned_agent_id ||
-      !!productEventData.changes.model_changes.assigned_group_id ||
-      !!productEventData.changes.model_changes.status ||
-      !!productEventData.changes.model_changes.priority ||
-      match !== undefined)
+    actor?.actor_source != null &&
+    actor.actor_source !== 'API' &&
+    !!model_changes &&
+    !!(assigned_agent_id || assigned_group_id || status || priority || match !== undefined)
   );
 };
