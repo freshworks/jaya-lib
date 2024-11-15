849fca999678ad0475ac6b483d3327b5baf1391a
ShashaankKrishnatray <shashaank.krishnatray@freshworks.com>
2024-11-15 14:01:56 +0530

    handling cases for events without changes field

--- packages/rule-engine/src/recommended/trigger-actions/update-conversation-property.ts
+++ packages/rule-engine/src/recommended/trigger-actions/update-conversation-property.ts
@@ -6,13 +6,13 @@ export default (productEvent: Event, productEventData: ProductEventData, trigger
   const { model_changes } = changes || {};
   const { assigned_agent_id, assigned_group_id, status, priority } = model_changes || {};
 
-  const match = Object.keys(model_changes).find((key) => /^cf_[a-zA-Z0-9_]+/.test(key));
+  const match = model_changes && Object.keys(model_changes).find((key) => /^cf_[a-zA-Z0-9_]+/.test(key));
 
   return (
     productEvent === Event.ConversationUpdate &&
     actor?.actor_source != null &&
     actor.actor_source !== 'API' &&
     !!model_changes &&
-    !!(assigned_agent_id || assigned_group_id || status || priority || match !== undefined)
+    !!(assigned_agent_id || assigned_group_id || status || priority || match)
   );
 };
